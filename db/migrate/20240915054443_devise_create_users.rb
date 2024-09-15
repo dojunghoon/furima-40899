ac1d5cbe249caf9056cd9cb84b4502dd642121f1
dojunghoon <y14wjdgns@naver.com>
2024-09-15 16:38:57 +0900

    d
    d

--- db/migrate/20240915054443_devise_create_users.rb
+++ db/migrate/20240915054443_devise_create_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateDeviseUsers < ActiveRecord::Migration[7.0]
+class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def self.up
     create_table :users do |t|
       ## Database authenticatable
