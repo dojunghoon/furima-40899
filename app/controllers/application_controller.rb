f78953fe38a1ecd16779721b72acd5f5c8d3233c
dojunghoon <y14wjdgns@naver.com>
2024-09-15 21:06:06 +0900

    d
    d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :authenticate_user!
 
   private
-
+  
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(
-      :sign_up, keys: [:nickname, :birth, :first_name, :last_name, :first_name_kana, :last_name_kana]
-    )
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :nickname, :email, :password, :password_confirmation,
+      :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date
+    ])
   end
 
   def basic_auth
