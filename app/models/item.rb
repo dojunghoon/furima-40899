312d2a3fdb3ef2a9fd4d46309d73d4ae0f1ac218
dojunghoon <y14wjdgns@naver.com>
2024-10-05 08:46:29 +0900

    後　pay

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,19 @@ class Item < ApplicationRecord
   validates :category_id, :condition_id, :shipping_cost_id, :prefecture_id, 
             :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank" } 
 
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is out of setting range" }
 
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :shipping_date_id
+    validates :prefecture_id
+
+end
+
+def sold_out?
+  self.buyer.present? # 'buyer'は購入者がいる場合に設定される関連です
+end
 
 end
